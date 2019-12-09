f208381214386b145dea4f29470ff8f9cb85c8f2
CodeMarciano <codemarciano@gmail.com>
2019-12-09 05:33:59 -0800

    Anidiendo Crud Empleado Lado del Backend consultas

--- api/routes/empleado-routes.js
+++ api/routes/empleado-routes.js
@@ -3,6 +3,9 @@ var empleadoControlador = require('../controllers/empleado-controller');
 var api = express();
 
 api.get('/get', empleadoControlador.get);
+api.post('/create', empleadoControlador.create);
+api.post('/update/:id', empleadoControlador.update);
+api.post('/delete/:id', empleadoControlador.destroy);
 api.get('/get/cantidadgenero', empleadoControlador.cantidadEmpleadoPorGenero);
 // api.post('/save', cargoControlador.save);
 // api.get('/update', cargoControlador.update);
